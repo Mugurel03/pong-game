7d5e7af9a57f722930764af77fac107fe0bf080b
coffe-machine <udrismugurel03@yahoo.com>
2024-02-13 21:02:34 +0200

    adding the ball class

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
+
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 
 screen = Screen()
-screen.setup(width=800, height=600)
+screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
 screen.bgcolor('black')
 screen.title('Pong')
 screen.tracer(0)
@@ -13,9 +19,12 @@ right_paddle = Paddle(350, 0)
 right_paddle.move(up_key="Up", down_key="Down")
 left_paddle = Paddle(-350, 0)
 left_paddle.move(up_key="w", down_key="s")
+ball = Ball()
 
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
-
+    ball.move()
+    ball.detect_collision(SCREEN_HEIGHT)
 
 screen.exitonclick()
